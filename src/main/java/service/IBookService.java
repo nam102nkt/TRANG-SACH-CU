3416089a7dd4081a354d6fbdf59a47457067adac
nam102nkt <nam102nkt@gmail.com>
2026-01-15 03:38:56 +0700

    Update IBookService.java

--- src/main/java/service/IBookService.java
+++ src/main/java/service/IBookService.java
@@ -2,7 +2,11 @@ package service;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.Map;
+
+import model.AuthorStats;
 import model.Book;
+import model.Category;
 
 public interface IBookService {
     List<Book> getFeaturedBooks();
@@ -13,4 +17,6 @@ public interface IBookService {
     void requestSellBook(Book b, int sellerId);
     // ADMIN
     void adminAddBook(Book b);
+    List<AuthorStats> getTopAuthors(int limit);
+    Map<Category, List<Book>> getBooksGroupedByCategory(int limitPerCategory);
 }
